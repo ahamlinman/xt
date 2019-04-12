357c3294aa4b968a64dc3a0a7e16aeef52f40a88
Alex Hamlin <alex@alexhamlin.co>
2019-04-11 22:16:13 -0700

    Set up basic CLI infrastructure
    So... all of the serializers and deserializers of the various libraries
    have different input / output requirements. Some take readers and
    writers, some only take strings... you get the idea. I'll have to figure
    out a decent generic API or whatever.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{crate_version, App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    App::new("recompose")
+        .version(crate_version!())
+        .about("Convert between serialized data formats")
+        .arg(
+            Arg::with_name("from")
+                .long("from")
+                .help("Format to convert from"),
+        )
+        .arg(
+            Arg::with_name("to")
+                .long("to")
+                .required(true)
+                .help("Format to convert to"),
+        )
+        .get_matches();
 }
