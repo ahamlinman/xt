b371f0904c1d6f2642bb9d522b083de5c3285fee
Alex Hamlin <alex@alexhamlin.co>
2021-03-06 18:06:12 -0800

    Implement minimal argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::str::FromStr;
+
+use structopt::StructOpt;
+
 fn main() {
-  println!("Hello from jyt v0.3.0!");
+  let opt = Opt::from_args();
+  println!("{:?}", opt);
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(about)]
+struct Opt {
+  #[structopt(short = "t", help = "Format to convert to", default_value = "json")]
+  to: Format,
+}
+
+#[derive(Debug)]
+enum Format {
+  Json,
+  Yaml,
+}
+
+impl FromStr for Format {
+  type Err = &'static str;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s {
+      "json" => Ok(Self::Json),
+      "yaml" => Ok(Self::Yaml),
+      _ => Err("unknown format"),
+    }
+  }
 }
