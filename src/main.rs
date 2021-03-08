69c92c63d6e5bd27b19cda4444e2d3d3b8ceb7e9
Alex Hamlin <alex@alexhamlin.co>
2021-03-07 17:27:12 -0800

    input and output variables
    This is, like, a really tiny step toward file support, at least for
    inputs.

--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,46 @@ use structopt::StructOpt;
 fn main() {
   let opt = Opt::from_args();
 
+  let input = io::stdin();
+  let output = io::stdout();
+
   match opt.from {
     None | Some(Format::Yaml) => {
-      let de = serde_yaml::Deserializer::from_reader(io::stdin());
-      transcode_and_write(de, opt.to);
+      let de = serde_yaml::Deserializer::from_reader(input);
+      transcode_to(de, opt.to, output);
     }
     Some(Format::Json) => {
-      let mut de = serde_json::Deserializer::from_reader(io::stdin());
-      transcode_and_write(&mut de, opt.to);
+      let mut de = serde_json::Deserializer::from_reader(input);
+      transcode_to(&mut de, opt.to, output);
     }
     Some(Format::Toml) => {
       let mut s = String::new();
-      io::stdin().lock().read_to_string(&mut s).unwrap();
+      input.lock().read_to_string(&mut s).unwrap();
       let mut de = toml::Deserializer::new(s.as_str());
-      transcode_and_write(&mut de, opt.to);
+      transcode_to(&mut de, opt.to, output);
     }
   }
 }
 
-fn transcode_and_write<'a, D>(de: D, to: Format)
+fn transcode_to<'a, D, W>(de: D, to: Format, mut output: W)
 where
   D: serde::de::Deserializer<'a>,
+  W: Write,
 {
   match to {
     Format::Json => {
-      let mut ser = serde_json::Serializer::pretty(io::stdout());
+      let mut ser = serde_json::Serializer::pretty(output);
       serde_transcode::transcode(de, &mut ser).expect("failed to serialize JSON")
     }
     Format::Yaml => {
-      let mut ser = serde_yaml::Serializer::new(io::stdout());
+      let mut ser = serde_yaml::Serializer::new(output);
       serde_transcode::transcode(de, &mut ser).expect("failed to serialize YAML")
     }
     Format::Toml => {
       let mut s = String::new();
       let mut ser = toml::Serializer::new(&mut s);
       serde_transcode::transcode(de, &mut ser).expect("failed to serialize TOML");
-      io::stdout().write(s.as_bytes()).unwrap();
+      output.write(s.as_bytes()).unwrap();
     }
   };
 }
