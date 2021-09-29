bbd7babb2e647757c0399f4d9a5d6e97497da6a9
Alex Hamlin <alex@alexhamlin.co>
2021-09-29 01:19:19 -0700

    Shuffle transcode things around for readability
    I want the proper transcoding logic to come before Value, since I want
    to think of Value as more of a hack that you use as a last resort. This
    is kind of like a form of marketing, I guess?

--- src/transcode.rs
+++ src/transcode.rs
@@ -8,171 +8,6 @@ use serde::{
   ser::{self, Serialize, SerializeMap, SerializeSeq, Serializer},
 };
 
-/// Implements methods of a [`serde::de::Visitor`] that do nothing more than
-/// shove the result of some expression into an `Ok` variant, using terms of the
-/// following form:
-///
-/// ```
-/// visit_<type>(<args>) => <return value>;
-/// ```
-macro_rules! impl_infallible_visitor_methods {
-  ($($name:ident($($args:tt)*) => $result:expr;)*) => {
-    $(
-      fn $name<E>($($args)*) -> ::std::result::Result<Self::Value, E>
-      where
-        E: ::serde::de::Error,
-      {
-        ::std::result::Result::Ok($result)
-      }
-    )*
-  };
-}
-
-/// Temporarily represents a deserialized value in memory.
-///
-/// On occasion, a Serde data format will not allow direct access to a usable
-/// `Serializer` or `Deserializer` for use with the [`transcode`] function, but
-/// will instead require the use of an intermediate (de)serializable value.
-/// `Value` is optimized to support this use case within jyt, distinguishing
-/// itself from alternative implementations by:
-///
-/// - Faithfully representing all Serde types that a jyt input format could
-///   produce (all integer sizes, raw bytes), but no more (options, etc.).
-/// - Borrowing from the deserializer whenever possible, avoiding new
-///   allocations but limiting the lifetime of the value.
-/// - Representing maps as a vector of 2-tuples, preserving the original order
-///   of values but avoiding the features (and overhead) of an order-preserving
-///   hash table.
-pub enum Value<'a> {
-  None,
-  Bool(bool),
-  I8(i8),
-  I16(i16),
-  I32(i32),
-  I64(i64),
-  I128(i128),
-  U8(u8),
-  U16(u16),
-  U32(u32),
-  U64(u64),
-  U128(u128),
-  F32(f32),
-  F64(f64),
-  Char(char),
-  String(Cow<'a, str>),
-  Bytes(Cow<'a, [u8]>),
-  Seq(Vec<Value<'a>>),
-  Map(Vec<(Value<'a>, Value<'a>)>),
-}
-
-impl Serialize for Value<'_> {
-  fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
-  where
-    S: Serializer,
-  {
-    use Value::*;
-    match self {
-      None => s.serialize_unit(),
-      Bool(b) => s.serialize_bool(*b),
-      I8(n) => s.serialize_i8(*n),
-      I16(n) => s.serialize_i16(*n),
-      I32(n) => s.serialize_i32(*n),
-      I64(n) => s.serialize_i64(*n),
-      I128(n) => s.serialize_i128(*n),
-      U8(n) => s.serialize_u8(*n),
-      U16(n) => s.serialize_u16(*n),
-      U32(n) => s.serialize_u32(*n),
-      U64(n) => s.serialize_u64(*n),
-      U128(n) => s.serialize_u128(*n),
-      F32(f) => s.serialize_f32(*f),
-      F64(f) => s.serialize_f64(*f),
-      Char(c) => s.serialize_char(*c),
-      String(v) => v.serialize(s),
-      Bytes(v) => v.serialize(s),
-      Seq(v) => v.serialize(s),
-      Map(m) => {
-        let mut map = s.serialize_map(Some(m.len()))?;
-        for (k, v) in m {
-          map.serialize_entry(k, v)?;
-        }
-        map.end()
-      }
-    }
-  }
-}
-
-impl<'de: 'a, 'a> Deserialize<'de> for Value<'a> {
-  fn deserialize<D>(d: D) -> Result<Self, D::Error>
-  where
-    D: Deserializer<'de>,
-  {
-    struct Visitor;
-
-    impl<'a> de::Visitor<'a> for Visitor {
-      type Value = Value<'a>;
-
-      fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "any supported value")
-      }
-
-      impl_infallible_visitor_methods! {
-        visit_unit(self) => Value::None;
-
-        visit_bool(self, v: bool) => Value::Bool(v);
-
-        visit_i8(self, v: i8) => Value::I8(v);
-        visit_i16(self, v: i16) => Value::I16(v);
-        visit_i32(self, v: i32) => Value::I32(v);
-        visit_i64(self, v: i64) => Value::I64(v);
-        visit_i128(self, v: i128) => Value::I128(v);
-
-        visit_u8(self, v: u8) => Value::U8(v);
-        visit_u16(self, v: u16) => Value::U16(v);
-        visit_u32(self, v: u32) => Value::U32(v);
-        visit_u64(self, v: u64) => Value::U64(v);
-        visit_u128(self, v: u128) => Value::U128(v);
-
-        visit_f32(self, v: f32) => Value::F32(v);
-        visit_f64(self, v: f64) => Value::F64(v);
-
-        visit_char(self, v: char) => Value::Char(v);
-
-        visit_str(self, v: &str) => Value::String(Cow::Owned(v.to_owned()));
-        visit_borrowed_str(self, v: &'a str) => Value::String(Cow::Borrowed(v));
-        visit_string(self, v: String) => Value::String(Cow::Owned(v));
-
-        visit_bytes(self, v: &[u8]) => Value::Bytes(Cow::Owned(v.to_owned()));
-        visit_borrowed_bytes(self, v: &'a [u8]) => Value::Bytes(Cow::Borrowed(v));
-        visit_byte_buf(self, v: Vec<u8>) => Value::Bytes(Cow::Owned(v));
-      }
-
-      fn visit_seq<V: de::SeqAccess<'a>>(self, mut v: V) -> Result<Self::Value, V::Error> {
-        let mut vec = match v.size_hint() {
-          None => Vec::new(),
-          Some(s) => Vec::with_capacity(s),
-        };
-        while let Some(e) = v.next_element()? {
-          vec.push(e)
-        }
-        Ok(Value::Seq(vec))
-      }
-
-      fn visit_map<V: de::MapAccess<'a>>(self, mut v: V) -> Result<Self::Value, V::Error> {
-        let mut vec = match v.size_hint() {
-          None => Vec::new(),
-          Some(s) => Vec::with_capacity(s),
-        };
-        while let Some(entry) = v.next_entry()? {
-          vec.push(entry)
-        }
-        Ok(Value::Map(vec))
-      }
-    }
-
-    d.deserialize_any(Visitor)
-  }
-}
-
 /// Transcodes from a Serde `Deserializer` to a Serde `Serializer`.
 ///
 /// Values produced by the `Deserializer` will be forwarded directly to the
@@ -234,6 +69,26 @@ where
       De(err) => Some(err),
     }
   }
+}
+
+/// Implements methods of a [`serde::de::Visitor`] that do nothing more than
+/// shove the result of some expression into an `Ok` variant, using terms of the
+/// following form:
+///
+/// ```
+/// visit_<type>(<args>) => <return value>;
+/// ```
+macro_rules! impl_infallible_visitor_methods {
+  ($($name:ident($($args:tt)*) => $result:expr;)*) => {
+    $(
+      fn $name<E>($($args)*) -> ::std::result::Result<Self::Value, E>
+      where
+        E: ::serde::de::Error,
+      {
+        ::std::result::Result::Ok($result)
+      }
+    )*
+  };
 }
 
 /// Enables a Serde `Deserializer` to drive the contained Serde `Serializer`.
@@ -437,5 +292,150 @@ where
         ))
       }
     }
+  }
+}
+
+/// Temporarily represents a deserialized value in memory.
+///
+/// On occasion, a Serde data format will not allow direct access to a usable
+/// `Serializer` or `Deserializer` for use with the [`transcode`] function, but
+/// will instead require the use of an intermediate (de)serializable value.
+/// `Value` is optimized to support this use case within jyt, distinguishing
+/// itself from alternative implementations by:
+///
+/// - Faithfully representing all Serde types that a jyt input format could
+///   produce (all integer sizes, raw bytes), but no more (options, etc.).
+/// - Borrowing from the deserializer whenever possible, avoiding new
+///   allocations but limiting the lifetime of the value.
+/// - Representing maps as a vector of 2-tuples, preserving the original order
+///   of values but avoiding the features (and overhead) of an order-preserving
+///   hash table.
+pub enum Value<'a> {
+  None,
+  Bool(bool),
+  I8(i8),
+  I16(i16),
+  I32(i32),
+  I64(i64),
+  I128(i128),
+  U8(u8),
+  U16(u16),
+  U32(u32),
+  U64(u64),
+  U128(u128),
+  F32(f32),
+  F64(f64),
+  Char(char),
+  String(Cow<'a, str>),
+  Bytes(Cow<'a, [u8]>),
+  Seq(Vec<Value<'a>>),
+  Map(Vec<(Value<'a>, Value<'a>)>),
+}
+
+impl Serialize for Value<'_> {
+  fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
+  where
+    S: Serializer,
+  {
+    use Value::*;
+    match self {
+      None => s.serialize_unit(),
+      Bool(b) => s.serialize_bool(*b),
+      I8(n) => s.serialize_i8(*n),
+      I16(n) => s.serialize_i16(*n),
+      I32(n) => s.serialize_i32(*n),
+      I64(n) => s.serialize_i64(*n),
+      I128(n) => s.serialize_i128(*n),
+      U8(n) => s.serialize_u8(*n),
+      U16(n) => s.serialize_u16(*n),
+      U32(n) => s.serialize_u32(*n),
+      U64(n) => s.serialize_u64(*n),
+      U128(n) => s.serialize_u128(*n),
+      F32(f) => s.serialize_f32(*f),
+      F64(f) => s.serialize_f64(*f),
+      Char(c) => s.serialize_char(*c),
+      String(v) => v.serialize(s),
+      Bytes(v) => v.serialize(s),
+      Seq(v) => v.serialize(s),
+      Map(m) => {
+        let mut map = s.serialize_map(Some(m.len()))?;
+        for (k, v) in m {
+          map.serialize_entry(k, v)?;
+        }
+        map.end()
+      }
+    }
+  }
+}
+
+impl<'de: 'a, 'a> Deserialize<'de> for Value<'a> {
+  fn deserialize<D>(d: D) -> Result<Self, D::Error>
+  where
+    D: Deserializer<'de>,
+  {
+    struct Visitor;
+
+    impl<'a> de::Visitor<'a> for Visitor {
+      type Value = Value<'a>;
+
+      fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "any supported value")
+      }
+
+      impl_infallible_visitor_methods! {
+        visit_unit(self) => Value::None;
+
+        visit_bool(self, v: bool) => Value::Bool(v);
+
+        visit_i8(self, v: i8) => Value::I8(v);
+        visit_i16(self, v: i16) => Value::I16(v);
+        visit_i32(self, v: i32) => Value::I32(v);
+        visit_i64(self, v: i64) => Value::I64(v);
+        visit_i128(self, v: i128) => Value::I128(v);
+
+        visit_u8(self, v: u8) => Value::U8(v);
+        visit_u16(self, v: u16) => Value::U16(v);
+        visit_u32(self, v: u32) => Value::U32(v);
+        visit_u64(self, v: u64) => Value::U64(v);
+        visit_u128(self, v: u128) => Value::U128(v);
+
+        visit_f32(self, v: f32) => Value::F32(v);
+        visit_f64(self, v: f64) => Value::F64(v);
+
+        visit_char(self, v: char) => Value::Char(v);
+
+        visit_str(self, v: &str) => Value::String(Cow::Owned(v.to_owned()));
+        visit_borrowed_str(self, v: &'a str) => Value::String(Cow::Borrowed(v));
+        visit_string(self, v: String) => Value::String(Cow::Owned(v));
+
+        visit_bytes(self, v: &[u8]) => Value::Bytes(Cow::Owned(v.to_owned()));
+        visit_borrowed_bytes(self, v: &'a [u8]) => Value::Bytes(Cow::Borrowed(v));
+        visit_byte_buf(self, v: Vec<u8>) => Value::Bytes(Cow::Owned(v));
+      }
+
+      fn visit_seq<V: de::SeqAccess<'a>>(self, mut v: V) -> Result<Self::Value, V::Error> {
+        let mut vec = match v.size_hint() {
+          None => Vec::new(),
+          Some(s) => Vec::with_capacity(s),
+        };
+        while let Some(e) = v.next_element()? {
+          vec.push(e)
+        }
+        Ok(Value::Seq(vec))
+      }
+
+      fn visit_map<V: de::MapAccess<'a>>(self, mut v: V) -> Result<Self::Value, V::Error> {
+        let mut vec = match v.size_hint() {
+          None => Vec::new(),
+          Some(s) => Vec::with_capacity(s),
+        };
+        while let Some(entry) = v.next_entry()? {
+          vec.push(entry)
+        }
+        Ok(Value::Map(vec))
+      }
+    }
+
+    d.deserialize_any(Visitor)
   }
 }
