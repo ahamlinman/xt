18e9f9d9b94075cdec469405cd114a44eaab31da
Alex Hamlin <alex@alexhamlin.co>
2021-10-01 16:40:07 -0700

    Add commments about test input restrictions

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,7 +1,21 @@
 use jyt::{jyt, Format, InputHandle};
 
+/// A single jyt test input.
+///
+/// The inputs for a given set of integration tests contain the same serialized
+/// content, as formatted by jyt itself. Translating any input to any format
+/// (including the source format itself) should produce the test input for that
+/// format, regardless of whether the format is auto detected or specified
+/// explicitly. This may impose limitations on the structure and values that the
+/// test input can contain.
 type TestInput = (Format, &'static [u8]);
 
+/// Test inputs for single document transcoding.
+///
+/// TOML's limitations impose several restrictions on these inputs. First, no
+/// input can contain a null value. Second, the root of each input must be a
+/// map. Third, the values in the map must appear in an order that TOML can
+/// support (non-tables appear before tables at a given level of nesting).
 const SINGLE_INPUTS: [TestInput; 4] = [
   (Format::Json, include_bytes!("single.json")),
   (Format::Yaml, include_bytes!("single.yaml")),
@@ -32,6 +46,11 @@ fn test_single_document_reader() {
   }
 }
 
+/// Test inputs for multi document transcoding.
+///
+/// The current auto detection logic for MessagePack imposes a restriction on
+/// these inputs: the root of the first input in the stream must be a map or
+/// array. Subsequent values may be of any supported type.
 const MULTI_INPUTS: [TestInput; 3] = [
   (Format::Json, include_bytes!("multi.json")),
   (Format::Yaml, include_bytes!("multi.yaml")),
