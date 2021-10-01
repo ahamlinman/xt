97112a04cc47a3ff3d26b6694a8fb391db99abef
Alex Hamlin <alex@alexhamlin.co>
2021-10-01 01:10:40 -0700

    Initial integration tests for single documents
    This ensures that we have consistent transcoding for every
    single-document input format between both byte slices and readers.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,2 +1,41 @@
+use jyt::{jyt, Format, InputHandle};
+
+type TestInput = (Format, &'static [u8]);
+
+const SINGLE_INPUTS: [TestInput; 4] = [
+  (Format::Json, include_bytes!("single.json")),
+  (Format::Yaml, include_bytes!("single.yaml")),
+  (Format::Toml, include_bytes!("single.toml")),
+  (Format::Msgpack, include_bytes!("single.msgpack")),
+];
+
 #[test]
-fn it_works() {}
+fn test_single_document_buffer_known_format() {
+  for ((from, input), (to, expected)) in all_input_combinations(&SINGLE_INPUTS) {
+    let input = InputHandle::from_buffer(input);
+    let mut output = Vec::new();
+    jyt(input, Some(from), to, &mut output).unwrap();
+    assert_eq!(&output, expected);
+  }
+}
+
+#[test]
+fn test_single_document_reader_known_format() {
+  for ((from, input), (to, expected)) in all_input_combinations(&SINGLE_INPUTS) {
+    let mut input = input;
+    let input = InputHandle::from_reader(&mut input);
+    let mut output = Vec::new();
+    jyt(input, Some(from), to, &mut output).unwrap();
+    assert_eq!(&output, expected);
+  }
+}
+
+fn all_input_combinations(inputs: &[TestInput]) -> Vec<(TestInput, TestInput)> {
+  let mut result = Vec::new();
+  for x in inputs {
+    for y in inputs {
+      result.push((*x, *y))
+    }
+  }
+  result
+}
