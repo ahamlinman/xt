323b0fa9e06601de4053af773ea807041170e847
Alex Hamlin <alex@alexhamlin.co>
2022-04-16 16:37:35 -0700

    Add JSON and TOML benchmarks; tweak parameters

--- benches/github_events.rs
+++ benches/github_events.rs
@@ -8,21 +8,74 @@ criterion_main!(benches);
 
 criterion_group! {
   name = benches;
-  config = Criterion::default()
-             .warm_up_time(Duration::from_secs(10))
-             .measurement_time(Duration::from_secs(60));
-  targets = bench_yaml_input, bench_msgpack_input
+  config = Criterion::default().measurement_time(Duration::from_secs(30));
+  targets = bench_json_input,
+            bench_yaml_input,
+            bench_toml_input,
+            bench_msgpack_input
+}
+
+fn bench_json_input(c: &mut Criterion) {
+  let mut group = c.benchmark_group("json");
+  let input = load_event_data(Format::Json);
+
+  group.bench_function("buffer_to_msgpack", |b| {
+    b.iter(|| {
+      xt::translate(
+        InputHandle::from_buffer(&*input),
+        Some(Format::Json),
+        Format::Msgpack,
+        std::io::sink(),
+      )
+    })
+  });
+
+  group.bench_function("reader_to_msgpack", |b| {
+    b.iter(|| {
+      xt::translate(
+        InputHandle::from_reader(&*input),
+        Some(Format::Json),
+        Format::Msgpack,
+        std::io::sink(),
+      )
+    })
+  });
+
+  group.finish();
 }
 
 fn bench_yaml_input(c: &mut Criterion) {
   let mut group = c.benchmark_group("yaml");
   let input = load_event_data(Format::Yaml);
 
-  group.bench_function("slice_to_json", |b| {
+  group.sample_size(50);
+
+  group.bench_function("buffer_to_json", |b| {
     b.iter(|| {
       xt::translate(
-        InputHandle::from_buffer(input.as_ref()),
+        InputHandle::from_buffer(&*input),
         Some(Format::Yaml),
+        Format::Json,
+        std::io::sink(),
+      )
+    })
+  });
+
+  group.finish();
+}
+
+fn bench_toml_input(c: &mut Criterion) {
+  let mut group = c.benchmark_group("toml");
+  let input = load_event_data(Format::Toml);
+
+  group.measurement_time(Duration::from_secs(60));
+  group.sample_size(20);
+
+  group.bench_function("buffer_to_json", |b| {
+    b.iter(|| {
+      xt::translate(
+        InputHandle::from_buffer(&*input),
+        Some(Format::Toml),
         Format::Json,
         std::io::sink(),
       )
@@ -36,10 +89,10 @@ fn bench_msgpack_input(c: &mut Criterion) {
   let mut group = c.benchmark_group("msgpack");
   let input = load_event_data(Format::Msgpack);
 
-  group.bench_function("slice_to_json", |b| {
+  group.bench_function("buffer_to_json", |b| {
     b.iter(|| {
       xt::translate(
-        InputHandle::from_buffer(input.as_ref()),
+        InputHandle::from_buffer(&*input),
         Some(Format::Msgpack),
         Format::Json,
         std::io::sink(),
