e424936259d7ab6971ed8360078da32153fba4c0
Alex Hamlin <alex@alexhamlin.co>
2023-01-31 20:05:04 -0800

    Reorganize TOML implementation and update comments
    First, don't split internal Output methods across two impl blocks, since
    that's just bad style.
    
    Second, update some details in comments that were only relevant with
    toml 0.5.

--- src/toml.rs
+++ src/toml.rs
@@ -58,6 +58,40 @@ impl<W: Write> Output<W> {
 	pub(crate) fn new(w: W) -> Output<W> {
 		Output { w, used: false }
 	}
+
+	fn ensure_one_use(&mut self) -> crate::Result<()> {
+		// Since TOML has no concept of multiple documents in a single stream,
+		// and we can't know the number of input documents in advance, we just
+		// fail if someone tries to use us more than once. We try to run this
+		// check before we even deserialize any values, so we don't waste time
+		// on things that will get thrown out.
+		if self.used {
+			return Err(TomlOutputError::MultiDocument.into());
+		}
+		self.used = true;
+		Ok(())
+	}
+
+	fn output_value(&mut self, value: &::toml::Value) -> crate::Result<()> {
+		// TOML requires that the root of the document be a table, which we
+		// can't guarantee for arbitrary input. While the toml crate allows
+		// deserializing to a toml::Table to avoid accepting non-table root
+		// values, the error message for such values (as of this writing) can be
+		// a bit noisier than the one we've historically provided ourselves, so
+		// we instead deserialize into the more general toml::Value and run the
+		// check manually.
+		if !value.is_table() {
+			return Err(TomlOutputError::NonTableRoot.into());
+		}
+
+		// toml::Serializer is smart enough to ensure that non-table values
+		// appear before any tables at a given nesting level, as TOML requires.
+		// However, this means that it will probably never be feasible to
+		// serialize directly to a writer.
+		let output_buf = ::toml::to_string_pretty(&value)?;
+		self.w.write_all(output_buf.as_bytes())?;
+		Ok(())
+	}
 }
 
 impl<W: Write> crate::Output for Output<W> {
@@ -66,24 +100,8 @@ impl<W: Write> crate::Output for Output<W> {
 		D: de::Deserializer<'de, Error = E>,
 		E: de::Error + Send + Sync + 'static,
 	{
-		// TOML is pretty unique among xt's supported output formats, and
-		// requires some special considerations.
-
-		// Since TOML has no concept of multiple documents in a single stream,
-		// and we can't know the number of input documents in advance, we just
-		// fail if someone tries to use us more than once. (We try to put this
-		// check first so we don't waste time on a value that will get thrown
-		// out.)
 		self.ensure_one_use()?;
-
-		// TOML requires that non-table values appear before any tables at a
-		// given nesting level, which we cannot guarantee for arbitrary input.
-		// toml::Value knows how to deal with this. We enable the toml crate's
-		// "preserve_order" feature to keep as much of the original input
-		// ordering as we can.
 		let value = ::toml::Value::deserialize(de)?;
-
-		// There are a couple more small details, see output_value for details.
 		self.output_value(&value)
 	}
 
@@ -91,7 +109,6 @@ impl<W: Write> crate::Output for Output<W> {
 	where
 		S: ser::Serialize,
 	{
-		// Of course, all of the above comments apply here as well.
 		self.ensure_one_use()?;
 		let value = ::toml::Value::try_from(value)?;
 		self.output_value(&value)
@@ -99,33 +116,6 @@ impl<W: Write> crate::Output for Output<W> {
 
 	fn flush(&mut self) -> io::Result<()> {
 		self.w.flush()
-	}
-}
-
-impl<W: Write> Output<W> {
-	fn output_value(&mut self, value: &::toml::Value) -> crate::Result<()> {
-		// From the spec: "TOML is designed to map unambiguously to a hash
-		// table." xt's other input formats can produce something like a boolean
-		// or array at the top level, which might make xt output an invalid TOML
-		// document if we're not careful. For example, I have seen the toml
-		// crate dump a top-level array as an array of tables with an empty
-		// name, i.e. with "[[]]" headers.
-		if !value.is_table() {
-			return Err(TomlOutputError::NonTableRoot.into());
-		}
-
-		// As of this writing, the toml crate can't output directly to a writer.
-		let output_buf = ::toml::to_string_pretty(&value)?;
-		self.w.write_all(output_buf.as_bytes())?;
-		Ok(())
-	}
-
-	fn ensure_one_use(&mut self) -> crate::Result<()> {
-		if self.used {
-			return Err(TomlOutputError::MultiDocument.into());
-		}
-		self.used = true;
-		Ok(())
 	}
 }
 
