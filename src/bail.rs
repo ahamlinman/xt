da0c314f0c290c60d9219a3db60062f42a40d7ca
Alex Hamlin <alex@alexhamlin.co>
2025-02-08 23:22:59 -0800

    Fix inconsistent indentation in bail.rs
    Happened to notice this and decided I might as well fix it.

--- src/bail.rs
+++ src/bail.rs
@@ -16,7 +16,7 @@ macro_rules! xt_bail {
 /// Formats a message to standard error that includes the provided file path,
 /// then terminates the current process with exit code 1.
 macro_rules! xt_bail_path {
-    ($path:expr, $fmt:literal $(, $($args:tt)* )?) => {{
+	($path:expr, $fmt:literal $(, $($args:tt)* )?) => {{
 		let _ = writeln!(
 			::std::io::stderr().lock(),
 			"xt error in {}: {}",
@@ -24,5 +24,5 @@ macro_rules! xt_bail_path {
 			format_args!($fmt $(, $($args)* )?),
 		);
 		::std::process::exit(1);
-    }};
+	}};
 }
