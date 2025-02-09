6685ec126773f7c0cd7143810826bfed1bdf19f2
Alex Hamlin <alex@alexhamlin.co>
2025-02-09 11:46:08 -0800

    Copyedit the pipecheck docs a bit
    I mentioned in the original commit message that I stole this idea from
    the Go runtime, but now I've actually added those references too.

--- src/pipecheck.rs
+++ src/pipecheck.rs
@@ -5,43 +5,53 @@ use std::io::{self, Write};
 /// A writer that silently terminates the program on broken pipe errors.
 ///
 /// When any call to its underlying writer returns a [`BrokenPipe`](io::ErrorKind::BrokenPipe)
-/// error, a `Writer` will immediately terminate the program without returning
-/// from the call. On Unix(-like) systems, the program will be terminated by a
-/// SIGPIPE signal. Otherwise, it will exit with code 1.
+/// error, a `Writer` terminates the current program with a SIGPIPE signal,
+/// or exits with code 1 on non-Unix systems.
 ///
 /// # Why is this useful?
 ///
-/// A Unix program that writes to a broken pipe will receive a SIGPIPE signal
-/// that, by default, terminates it immediately. This can be convenient for
-/// simple CLI tools used in shell pipelines, where this is a common error that
-/// is best handled by exiting silently, unlike most other errors that should be
-/// reported to the user. However, this default can be problematic for more
-/// complex applications, especially networked servers that could write to a
-/// broken pipe any time a client unexpectedly closes or drops a connection.
+/// A Unix program that writes to a broken pipe receives a SIGPIPE signal that,
+/// by default, terminates it immediately. This is usually the optimal behavior
+/// for simple CLI tools used in shell pipelines, but is problematic for
+/// applications like networked servers that may write to a broken pipe any time
+/// a client unexpectedly closes or drops a connection.
 ///
-/// Unlike C programs, Rust programs on Unix(-like) systems ignore SIGPIPE by
-/// default, and instead return a normal [`Result::Err`] for writes to broken
-/// pipes. While there are good reasons that this is *not* actually a great
-/// default, it provides valuable consistency with non-Unix platforms and is
-/// helpful in the server use case described above. Unfortunately, experience
-/// shows that real-world Rust libraries do not always propagate I/O errors in a
-/// manner that makes them easy to consistently identify, e.g. by including I/O
-/// error values in error source chains. This can make it difficult to implement
-/// the desirable CLI behavior of exiting silently on broken pipes while loudly
-/// reporting other errors.
+/// Rust overrides the Unix default during program initialization (before `main`
+/// is called), ignoring SIGPIPE so that writes to broken pipes consistently
+/// return a normal [`Result::Err`]. This is helpful for the server use case
+/// described above and provides valuable consistency with non-Unix platforms,
+/// even though it severely hampers some specialized use cases on Unix.
 ///
-/// `Writer` provides the benefits of the default Unix SIGPIPE behavior in a
-/// more Rust-friendly and cross-platform manner. On Unix(-like) systems in
-/// particular, `Writer` terminates the program using the default SIGPIPE
-/// handler for full consistency with typical Unix CLI tools, but without
-/// modifying global SIGPIPE behavior up front in a way that might conflict with
-/// other use cases.
+/// Unfortunately, experience shows that real-world Rust libraries don't
+/// consistently propagate broken pipe errors in a generically detectable way,
+/// such as by including them in error source chains. This makes it difficult
+/// for well-meaning CLIs to replicate the behavior of Unix's default SIGPIPE
+/// handler in a cross-platform manner. `Writer` solves this by plumbing the
+/// required termination logic directly into every write operation, before it
+/// can get lost on its way up the call stack.
 ///
-/// For further background on Rust's handling of SIGPIPE, see:
+/// On Unix systems in particular, `Writer` accomplishes this by modifying the
+/// process-wide behavior of SIGPIPE immediately before raising that signal
+/// against the current thread, rather than requiring its modification for all
+/// writes in the program. It is also important to note that Unix distinguishes
+/// between process termination by a signal and process termination with an exit
+/// code; `Writer` does not employ incorrect hacks like exiting with code 141
+/// (attempting to replicate the return code of a command terminated by SIGPIPE
+/// in a shell).
+///
+/// For further background on SIGPIPE, Rust's handling of it, and cross-platform
+/// portability concerns surrounding broken pipes, see:
 ///
 /// - <https://github.com/rust-lang/rust/issues/62569>
 /// - <https://stackoverflow.com/a/65760807>
 /// - <https://github.com/BurntSushi/ripgrep/issues/200#issuecomment-616884727>
+///
+/// For the implementation in the Go programming language that directly inspired
+/// this approach, see:
+///
+/// - <https://pkg.go.dev/os/signal#hdr-SIGPIPE>
+/// - <https://cs.opensource.google/go/go/+/refs/tags/go1.23.6:src/os/file_unix.go;l=252>
+/// - <https://cs.opensource.google/go/go/+/refs/tags/go1.23.6:src/runtime/signal_unix.go;l=333>
 pub(crate) struct Writer<W>(W)
 where
 	W: Write;
