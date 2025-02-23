276c7bb9860182000bb09b9cb66439bc04a9c2ff
Alex Hamlin <alex@alexhamlin.co>
2025-02-23 12:38:58 -0800

    Add a fuzz target for readers
    No issues so far on this one, thankfully. I'd hate to have to cut a
    third release in two days.

---
+++ fuzz/fuzz_targets/reader_detected_all_formats.rs
@@ -0,0 +1,14 @@
+#![no_main]
+
+use std::io;
+
+use libfuzzer_sys::fuzz_target;
+
+use xt::Format;
+
+fuzz_target!(|data: &[u8]| {
+	let _ = xt::translate_reader(&*data, None, Format::Json, io::sink());
+	let _ = xt::translate_reader(&*data, None, Format::Msgpack, io::sink());
+	let _ = xt::translate_reader(&*data, None, Format::Toml, io::sink());
+	let _ = xt::translate_reader(&*data, None, Format::Yaml, io::sink());
+});
